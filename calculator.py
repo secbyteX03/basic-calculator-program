2d09af52c30b36315949749cd7dd03e83703ce6d
secbyteX03 <faithmagret92@gmail.com>
2025-07-31 22:19:03 +0300

    Prompt user for operation input

--- calculator.py
+++ calculator.py
@@ -3,3 +3,5 @@
 num1 = float(input("Enter the first number: "))
 # Get the second number from user
 num2 = float(input("Enter the second number: "))
+# Get the operation from user
+operation = input("Enter operation (+, -, *, /): ")
