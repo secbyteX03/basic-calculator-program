f16db93c9594bd183a230b5e94a787ed7d186c02
secbyteX03 <faithmagret92@gmail.com>
2025-07-31 22:18:07 +0300

    Add input for the second number

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
 # Basic Calculator Program
 # Get the first number from user
 num1 = float(input("Enter the first number: "))
+# Get the second number from user
+num2 = float(input("Enter the second number: "))
